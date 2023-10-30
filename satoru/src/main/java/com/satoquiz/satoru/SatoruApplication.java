6966000c19cbbb0f7cb5018518c55c94af842b4d
mjsimo99 <mohashark21@gmail.com>
2023-10-30 17:30:47 +0100

    ipdate

---
+++ satoru/src/main/java/com/satoquiz/satoru/SatoruApplication.java
@@ -0,0 +1,13 @@
+package com.satoquiz.satoru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatoruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatoruApplication.class, args);
+	}
+
+}
---
+++ satoru/src/test/java/com/satoquiz/satoru/SatoruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satoquiz.satoru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatoruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
